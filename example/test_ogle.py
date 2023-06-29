6b38a201302828eadfd440b99818ac0076e5e570
Michael Wood-Vasey <wmwv@pitt.edu>
2023-06-29 15:16:41 +0000

    Control where files get saved to.

--- example/test_ogle.py
+++ example/test_ogle.py
@@ -25,22 +25,22 @@ def load_ogle_timestamps(path="OGLE_II", suffix="*.dat"):
     return timestamps
 
 
-def train(dirname=os.getenv("HOME")):
+def train(dirname=os.getenv("HOME"), name="test"):
     timestamps = load_ogle_timestamps()
 
     data_x, data_y = training_set.create(timestamps, min_mag=15, max_mag=20, n_class=50)
 
     # By default the file is saved in the home directory
     data = np.loadtxt(
-        os.path.join(dirname) + "all_features.txt", dtype=str, comments="#"
+        os.path.join(dirname, "all_features_.txt"), dtype=str, comments="#"
     )
 
     data_x = data[:, 2:].astype("float")
     data_y = data[:, 0]
 
     model = ensemble_model.Classifier(data_x, data_y, n_iter=25, boruta_trials=25)
-    model.create()
-    model.save()
+    model.create(filename=name)
+    model.save(filename=name)
 
 
 def predict_ogle(model, path="OGLE_II", suffix="*.dat"):
@@ -53,14 +53,11 @@ def predict_ogle(model, path="OGLE_II", suffix="*.dat"):
     return prediction
 
 
-def run(csv_file=os.path.join(os.getenv("HOME"), "MicroLIA_Training_Set__.csv")):
-    train()
-
-
-#    model = ensemble_model.classifier()
-#    predict_ogle(model)
+def run(csv_file=os.path.join(os.getenv("HOME"), "MicroLIA_Training_Set_test_.csv")):
+#    train()
+    model = ensemble_model.Classifier(csv_file)
+    predict_ogle(model)
 
 
 if __name__ == "__main__":
-    #    train()
     run()
